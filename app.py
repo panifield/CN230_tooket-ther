5e43e348e0909170e610e2b84ffde27c2a0d52b7
panifield <finonsay2004@gmail.com>
2026-04-27 00:20:07 +0700

    feat: add Vercel deployment URL to CORS allowed origins

--- app.py
+++ app.py
@@ -140,7 +140,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://cn-230-tooket-ther-z8ld.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
